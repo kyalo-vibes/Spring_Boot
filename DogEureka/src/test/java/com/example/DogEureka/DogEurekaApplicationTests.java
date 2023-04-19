8b9be4eb80e3ba7b0c7c7630548218309872075f
Kyalo <kevin.kioko4@gmail.com>
2023-04-19 18:14:25 +0300

    Finished eureka

---
+++ DogEureka/src/test/java/com/example/DogEureka/DogEurekaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.DogEureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+
+@SpringBootTest
+class DogEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
